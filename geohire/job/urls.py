69329f9195ff64cf8dd4b974138e55b9fa7568e4
Mahmud, Sumaiya <smahmud38@gatech.edu>
2025-09-16 18:07:04 -0400

    Update urls.py

--- geohire/job/urls.py
+++ geohire/job/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='job.index'),
+    path('create/', views.create_job, name='create_job'),
+    path('edit/<int:job_id>/', views.edit_job, name='edit_job'),
 ]
