f2815eef6fb5117f371207dc47cb9dd0d633d53b
Mahmud, Sumaiya <smahmud38@gatech.edu>
2025-09-16 18:22:39 -0400

    Update forms.py

--- geohire/job/forms.py
+++ geohire/job/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from .models import Job
 
 class JobForm(forms.ModelForm):
-  class Meta:
-    model = Job
-    fields = ['title', 'description', 'location']
+    class Meta:
+        model = Job
+        fields = ['title', 'description', 'location']
