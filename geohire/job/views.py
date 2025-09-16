14f3689942a669f7269c4e0f28278a3fb4bde9be
Alaric006 <alaricjenkins@outlook.com>
2025-09-15 22:20:17 -0400

    Modified base.html, added to relevant pages

--- geohire/job/views.py
+++ geohire/job/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template_data = {}
+    template_data['title'] = "Job"
     return render(request, "job/index.html", {"template_data": template_data})
