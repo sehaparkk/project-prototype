6d1f486a3e3a9eaabea10aa535bf16e16891d58a
Andrew <50636418+Andrewbandrew05@users.noreply.github.com>
2025-09-16 14:47:44 -0400

    Moved logout to geohire
    Moved the logout page to the geohire app to reduce code duplication.

--- geohire/geohire/urls.py
+++ geohire/geohire/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     path('job/', include('job.urls')), # links job app
     path('recruiter/', include('recruiter.urls')), # links recruiter app
     path('login/', views.login, name='login'),  # Custom login view
+    path('logout/', auth_views.LogoutView.as_view(next_page='homepage'), name='logout'),
 ]
 
 #this allows to app the server media files during development (it basically makes them static)
