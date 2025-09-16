a6616934fbd76cebb581746bf397d46e1b717577
Mahmud, Sumaiya <smahmud38@gatech.edu>
2025-09-16 17:56:44 -0400

    Update models.py

--- geohire/job/models.py
+++ geohire/job/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
