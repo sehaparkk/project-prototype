2b5bd160199fc631f662b63cf236b87fdd09c134
Mahmud, Sumaiya <smahmud38@gatech.edu>
2025-09-17 16:58:16 -0400

    Update models.py

--- geohire/job/models.py
+++ geohire/job/models.py
@@ -1,19 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Job(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    location = models.CharField(max_length=100)
     posted_at = models.DateTimeField(auto_now_add=True)
-    skills = models.TextField(default="Not specified") 
-    salary_range = models.CharField(max_length=50, default="Not specified")
+
+    salary_min = models.IntegerField(null=True, blank=True)
+    salary_max = models.IntegerField(null=True, blank=True)
+    hide_salary = models.BooleanField(default=False)
+
     remote_or_onsite = models.CharField(
         max_length=20,
         choices=[("Remote", "Remote"), ("Onsite", "Onsite")],
         default="Remote"
     )
-
     visa_sponsorship = models.BooleanField(default=False)
+
+    skills = models.ManyToManyField(Skill, related_name="jobs")
+
     def __str__(self):
         return self.title
+
+
+class JobLocation(models.Model):
+    job = models.OneToOneField(Job, on_delete=models.CASCADE, related_name="location")
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    zip_code = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.job.title} - {self.country} at {self.zip_code}"
